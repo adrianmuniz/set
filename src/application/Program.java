8d7198971d71009dca0e71ec3208fd7405baf4f2
Adrian Muniz Da Silva <adrianmuniz.ceo@outlook.com>
2022-02-08 16:56:38 -0300

    Set e implementações

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Set<String> set = new LinkedHashSet<String>();
+		
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		set.removeIf(x -> x.length() >= 3);
+		
+		for (String p : set) {
+		System.out.println(p);
+		}
+	}
+}
